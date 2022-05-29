212cdcf489dfdea53a6a79c796173bd3126ce565
Jose <jodiaz198@yahoo.com>
2022-05-28 23:30:23 -0700

    modifications

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 let x = "jose"
 
 console.log(x)
+
+console.log('test, test, test')
+
+
+// var timer = null;
+// $('#text').keydown(function(){
+//        clearTimeout(timer); 
+//        timer = setTimeout(doStuff, 1000)
+// });
+
+// function doStuff() {
+//     alert('do stuff');
+// }
+
+// let timer = null;
+
+
+
+
+function startTimer() {
+    console.log('hello')
+    let btn = document.getElementById('timer-btn-1')
+    btn.classList.add('.timer')
+}
